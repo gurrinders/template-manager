c821f412b7ceaf9ffea3f0c1b254b752b0388962
Joe <joel.dcunha88@gmail.com>
2020-12-21 14:28:07 -0500

    Added code for navigation menu and Routing

--- poc-template/src/app/app-routing.module.ts
+++ poc-template/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchTemplateComponent } from '../app/search-template/search-template.component';
+import { PreviewTemplateComponent } from '../app/preview-template/preview-template.component';
+import { LoginTemplateComponent } from '../app/login-template/login-template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'login', component: LoginTemplateComponent },
+    { path: 'searchTemplate', component: SearchTemplateComponent },
+    { path: 'previewTemplate', component: PreviewTemplateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
